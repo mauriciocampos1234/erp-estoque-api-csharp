9af3d360531b6047f2b9f5380d64f7c41929ebfa
mauriciocampos1234 <mauriciocampospaypal@gmail.com>
2026-08-03 15:16:43 -0300

    feat: configuracao do entity framework core, appdbcontext e appsettings

--- ErpEstoque.Api/Models/Categoria.cs
+++ ErpEstoque.Api/Models/Categoria.cs
@@ -1,6 +1,17 @@
-﻿namespace ErpEstoque.Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ErpEstoque.Api.Models
 {
     public class Categoria
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome da categoria é obrigatório.")]
+        [MaxLength(100)]
+        public string Nome { get; set; } = string.Empty;
+
+        [MaxLength(255)]
+        public string? Descricao { get; set; }
     }
 }
--- ErpEstoque.Api/Models/Produto.cs
+++ ErpEstoque.Api/Models/Produto.cs
@@ -1,6 +1,29 @@
-﻿namespace ErpEstoque.Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ErpEstoque.Api.Models
 {
     public class Produto
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome do produto é obrigatório.")]
+        [MaxLength(150)]
+        public string Nome { get; set; } = string.Empty;
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Preco { get; set; }
+
+        public int QuantidadeEstoque { get; set; } = 0;
+
+        // Chave Estrangeira (O Id da categoria no banco de dados)
+        [Required]
+        public int CategoriaId { get; set; }
+
+        // Propriedade de Navegação (Para o C# entender o relacionamento na memória)
+        [ForeignKey("CategoriaId")]
+        public virtual Categoria? Categoria { get; set; }
     }
 }
