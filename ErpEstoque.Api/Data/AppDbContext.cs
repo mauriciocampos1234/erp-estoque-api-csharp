92c6f70252a92278616a6e0023f8f2517c70a6a4
mauriciocampos1234 <mauriciocampospaypal@gmail.com>
2026-08-03 15:32:28 -0300

    fix: move pasta Data para dentro do projeto API

---
+++ ErpEstoque.Api/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ErpEstoque.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ErpEstoque.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
